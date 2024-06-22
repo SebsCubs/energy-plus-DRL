60a67217ea8bac7347078d9add1d7064ac8dd7c4
SebsCubs <ascespedesc@unal.edu.co>
2024-06-22 13:32:12 +0200

    Reorganizing folder structure to have clearer examples

--- eplus_drl/__init__.py
+++ eplus_drl/__init__.py
@@ -1,2 +1,3 @@
 from .emspy import EmsPy
 from .bca import BcaEnv
+from . import utils
---
+++ eplus_drl/utils.py
@@ -0,0 +1,23 @@
+import configparser
+
+def load_config(config_file='config.ini'):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    
+    config_dict = {
+        'ep_path': config['DEFAULT']['ep_path'],
+        'idf_file_name': config['DEFAULT']['idf_file_name'],
+        'ep_weather_path': config['DEFAULT']['ep_weather_path'],
+        'cvs_output_path': config['DEFAULT']['cvs_output_path'],
+        'number_of_subprocesses': config.getint('DEFAULT', 'number_of_subprocesses'),
+        'number_of_episodes': config.getint('DEFAULT', 'number_of_episodes'),
+        'eplus_verbose': config.getint('DEFAULT', 'eplus_verbose'),
+        'state_size': tuple(map(int, config['DEFAULT']['state_size'].split(','))),
+        'action_size': config.getint('DEFAULT', 'action_size'),
+        'learning_rate': config.getfloat('DEFAULT', 'learning_rate'),
+        'model_path': config['DEFAULT']['model_path'],
+        'queue_size_max' : config.getint('DEFAULT', 'queue_size_max'),
+        'show_plots' : config.getboolean('DEFAULT', 'show_plots')
+    }
+    
+    return config_dict
